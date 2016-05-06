5ae1241ecd35a750071441e752a0ecdf39c5dbb8
lxndr <lxndr87i@gmail.com>
2016-05-06 10:29:10 +0300

    add utils

---
+++ src/util.js
@@ -0,0 +1,43 @@
+import _ from 'lodash';
+
+function _merge(target, source, path, cb) {
+  if (target === source) {
+    return;
+  }
+
+  _.each(source, (value, key) => {
+    const valuePath = path.concat([key]);
+    if (_.isObject(value)) {
+      if (!_.isObject(target[key])) {
+        target[key] = {};
+      }
+      _merge(target[key], value, valuePath, cb);
+    } else {
+      cb(valuePath, value);
+      target[key] = value;
+    }
+  });
+}
+
+/**
+ * Lo-dash mergeWith is not good enough.
+ * @param {Object} target
+ * @param {Object|Object[]} sources
+ * @param {Function} cb
+ */
+export function merge(target, sources, cb) {
+  if (!_.isArray(sources)) {
+    sources = [sources];
+  }
+
+  _.each(sources, source => {
+    _merge(target, source, [], cb);
+  });
+}
+
+/**
+ * Checks if target array starts with array.
+ */
+export function startsWith(target, array) {
+  return _.every(array, (item, index) => target[index] === item);
+}
